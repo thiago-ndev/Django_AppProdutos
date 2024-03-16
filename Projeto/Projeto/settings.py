1225e31ab64e84a72e56c1736cb3073f552b9ce2
Thiago Alves <thiago.ndev@gmail.com>
2024-03-16 20:02:05 -0300

    feat: adicionado um novo sidebar interativo e novos updates nos estilos principais

--- Projeto/Projeto/settings.py
+++ Projeto/Projeto/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'appProdutos',
     'appClientes',
     'plataforma',
+    'mathfilters'
 
 ]
 
---
+++ Projeto/appProdutos/static/appProdutos/js/breakUserName.js
@@ -0,0 +1,6 @@
+var sideBarUsername = document.getElementById('user_side_bar').textContent
+document.getElementById('user_side_bar').textContent = sideBarUsername.split('@')[0]
+
+
+
+
